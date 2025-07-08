de693d90c968d9b32ac07e3db29f71b9e9fad621
jayaraju <k.jayaraju14@gmail.com>
2025-07-08 23:59:19 +0530

    basic http methods

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
 const express=require('express');
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log("this is middleware")
-    next()
-})
+const userData=[
+    {name:"jayaraju",role:"fullstack developer"},
+    {name:'karunakar',role:"test Enginer"}
+]
 
-app.get('/',(req,res)=>{
-    console.log(req.params)
-    res.send("this is first api")
+app.get('/users',(req,res)=>{
+res.send(userData)
+})
+app.post('/user',(req,res)=>{
+    try
+    {
+data={name:'sidhu',role:'MERN stack developer'}
+    const update=userData.push(data)
+    res.send(userData)
+}
+    catch(err)
+    {
+        console.log(err)
+    }
+    })
+    app.delete('/user',(req,res)=>{
+        userData.pop()
+        res.send(userData)
+    })
+app.use('/test',(req,res,next)=>{
+    console.log("this is testing")
+    next()
 })
 
 app.listen(3000,()=>{
